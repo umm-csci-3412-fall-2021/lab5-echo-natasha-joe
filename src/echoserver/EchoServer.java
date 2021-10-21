51355cb4edcaf137454295f3b1cfc16b0a22ba0d
Joe Walbran <walbr037@morris.umn.edu>
2021-10-21 11:03:43 -0500

    Implement echo client
    We also made some small changes to the server code--notably, changing
    how we test when the input from the socket is finished. (The old version
    did the "is the input finished" check and "read from the input" step
    separately, and we were worried there might be race conditions if, eg,
    the input was closed between our check and our read.)
    
    The lab should be all finished and working at this point.
    
    Co-authored-by: Natasha-Zebrev <zebre001@morris.umn.edu>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,7 +7,7 @@ public class EchoServer {
   public static final int PORT_NUMBER = 6013;
   public static final int BUFFER_SIZE = 1024;
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     try {
       ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 
@@ -17,12 +17,16 @@ public class EchoServer {
         OutputStream out = socketToClient.getOutputStream();
         byte[] buffer = new byte[BUFFER_SIZE];
 
-        while (!socketToClient.isInputShutdown()) {
-          int bytesRead = in.read(buffer);
+        // Read from the socket and echo any data back.
+        // When the client closes their "output" half of the socket, end the
+        // loop.
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
           out.write(buffer, 0, bytesRead);
           out.flush();
         }
 
+        // Then, close our "output" half of the socket.
         socketToClient.shutdownOutput();
       }
     } catch (IOException ioe) {
