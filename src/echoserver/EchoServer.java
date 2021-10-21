75a70214cd43214d1c2310f46a8fbe95ab02fc9f
Joe Walbran <walbr037@morris.umn.edu>
2021-10-21 10:24:48 -0500

    Implement echo server
    The client isn't written yet, but we expect the server process to work
    correctly now.
    
    Co-authored-by: Natasha-Zebrev <zebre001@morris.umn.edu>

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,33 @@
 package echoserver;
 
+import java.net.*;
+import java.io.*;
+
 public class EchoServer {
+  public static final int PORT_NUMBER = 6013;
+  public static final int BUFFER_SIZE = 1024;
+
+  public static void main(String[] args) throws IOException {
+    try {
+      ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+
+      while (true) {
+        Socket socketToClient = serverSocket.accept();
+        InputStream in = socketToClient.getInputStream();
+        OutputStream out = socketToClient.getOutputStream();
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        while (!socketToClient.isInputShutdown()) {
+          int bytesRead = in.read(buffer);
+          out.write(buffer, 0, bytesRead);
+          out.flush();
+        }
+
+        socketToClient.shutdownOutput();
+      }
+    } catch (IOException ioe) {
+      System.err.println("Unexpected exception:");
+      ioe.printStackTrace();
+    }
+  }
 }
